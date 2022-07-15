909f28ea4930d8d23eac661a3af9ac733172deb6
NatiG <nati.g5684@gmail.com>
2022-07-16 01:30:48 +0300

    Create link tables

--- schema.sql
+++ schema.sql
@@ -28,3 +28,31 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+	id	INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
+	age INT,
+	date_of_graduation date,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+	id	INT GENERATED ALWAYS AS IDENTITY,
+	species_id INT,
+	vets_id INT,
+	PRIMARY KEY(id)
+);
+
+ALTER TABLE specializations ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE specializations ADD FOREIGN KEY (vets_id) REFERENCES vets(id);
+
+CREATE TABLE visits (
+	id	INT GENERATED ALWAYS AS IDENTITY,
+	animals_id INT,
+	vets_id INT,
+	PRIMARY KEY(id)
+);
+
+ALTER TABLE visits ADD FOREIGN KEY (animals_id) REFERENCES animals(id);
+ALTER TABLE visits ADD FOREIGN KEY (vets_id) REFERENCES vets(id);
